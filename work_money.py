3eebfb14c1647375f9c6dff44f3277c6476c72b0
jhjh81704 <jhjh81704@gmail.com>
2019-05-09 21:11:12 +0800

    時薪 & 月薪 test_ver

--- work_money.py
+++ work_money.py
@@ -6,56 +6,69 @@ Created on Tue Apr  2 22:35:23 2019
 """
 
 
-class money(object):  # 計算月薪
-    
-    def __init__(self, money_hr, hr, day):        
-        self.money_hr = money_hr
+class Money(object):  # 計算月薪    
+    def __init__(self, hr, day):        
         self.hr = hr
-        self.day = day
+        self.day = day       
     
+    def getMoney_hr(self): # 時薪
+        return self.Money_hr
+    
+    def setMoney_hr(self, Money_hr):
+        self.Money_hr = Money_hr
+        
     def __str__(self):
         """
-        月薪:  = 時薪 (money_hr) * 小時 (hr) * 天數 (day)
+        月薪 = 時薪(self.Money_hr) * 小時(self.hr) * 天數(self.day)
         """
-        return str('月薪' + str(self.money_hr * self.hr *self.day))
+        return '月薪 =' + str(float(self.Money_hr * self.hr * self.day))
     
     def __sub__(self, other):  # 兩份工作行情的比較
-        return str(float(self.money_hr * self.hr *self.day \
-                        - other.money_hr * other.hr *other.day))
+        return str(float(self.Money_hr * self.hr *self.day \
+                        - other.Money_hr * other.hr *other.day))
+
 
  ##使用參考指令:
-a = money(120, 9, 26)
-b = money(150, 9, 22)
+a = Money(9, 26)
+b = Money(9, 22)
+a.setMoney_hr(120)
+b.setMoney_hr(150)
 print(a)
 print(b)
-print('相差' + (a.__sub__(b)))  # 計算兩份工作行情的比較
+print('相差: ' + (a.__sub__(b)))  # 計算兩份工作行情的比較
 
 
 #--------------------------------------------------------------------
  
 
 # 計算時薪的這支程式碼有錯誤，但目前找不到問題出在哪裡......
-class money_hr(money):  # 計算時薪
+class Money_hr(Money):  # 計算時薪
 
-    def __inti__(self, money_month, hr, day):
-       
-        money._inti__(self, hr, day)
-        self.money_month = money_month
-    
-
-    def __str__(self):
-        """
-        時薪: 月薪 (money_month) / 小時 (hr) * 天數 (day)
-        """
-        return '時薪' + str(self.money_month / (self.hr *self.day))
+   def __inti__(self, hr, day):
+      
+       Money.__inti__(self, hr, day)       
    
-    def __sub__(self, other):  # 兩份工作行情的比較
-        return str(self.money_month / (self.hr *self.day) \
-                         - other.money_month / (other.hr *other.day))
+   def getMoney_month(self):
+       return self.Money_month
+   
+   def setMoney_month(self, Money_month):
+       self.Money_month = Money_month
 
- #使用參考指令:
-a = money_hr(28000, 9, 26)
-b = money_hr(25000, 9, 22)
-print(a)
-print(b)
-print('相差' + (a.__sub__(b))) # 計算兩份工作行情的比較
+   def __str__(self):
+       """
+       時薪 = 月薪 (self.Money_month) / 小時 (self.hr) * 天數 (self.day)
+       """
+       return '時薪 =' + str(float(self.Money_month / (self.hr *self.day)))
+  
+   def __sub__(self, other):  # 兩份工作行情的比較
+       return str(float(self.Money_month / (self.hr *self.day) \
+                        - other.Money_month / (other.hr *other.day)))
+
+#使用參考指令:
+c = Money_hr(9, 26)
+d = Money_hr(9, 22)
+c.setMoney_month(28000)
+d.setMoney_month(25000)
+print(c)
+print(d)
+print('相差:' + (a.__sub__(b))) # 計算兩份工作行情的比較
